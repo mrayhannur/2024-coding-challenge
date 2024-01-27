6c6b34b925346d3e5ab6b2b14fda0dda45092034
Rayhan <muhammadrayhannur9@gmail.com>
2024-01-28 04:34:12 +0700

    4/366

---
+++ 4-math_basic.py
@@ -0,0 +1,25 @@
+# 2024 Coding Challenge
+# January 28, 2024 (4/366)
+# https://www.codewars.com/kata/55a5befdf16499bffb00007b/
+
+def add(a,b):
+    return a+b
+
+# Make more functions. Refer to the description for function names.
+# The code will NOT WORK IF YOU USE names other than the ones
+# from the description
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def mod(a,b):
+    return a%b
+
+def exponent(a,b):
+    return a**b
+
+def subt(a,b):
+    return a-b
