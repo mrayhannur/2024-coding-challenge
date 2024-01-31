d37976e7adf5b7d2076aab8a7b4a9600b3ad6f01
Rayhan <muhammadrayhannur9@gmail.com>
2024-02-01 05:35:34 +0700

    11/365

---
+++ 11-replace_dots.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# February 1, 2024 (11/366)
+# https://www.codewars.com/kata/596c6eb85b0f515834000049/
+
+# My Own
+def replace_dots(s):
+    return s.replace('.', '-') # Replace . character in string with -
+
