0c685bf70c62bb2b657d8ac761b533cd8eb484e4
Rayhan <muhammadrayhannur9@gmail.com>
2024-02-06 06:55:03 +0700

    025/366

---
+++ 025-reverse_list.py
@@ -0,0 +1,9 @@
+# 2024 Coding Challenge
+# February 6, 2024 (25/366)
+# https://www.codewars.com/kata/57a04da9e298a7ee43000111/
+
+# Desc: Write reverseList function that simply reverses lists.
+
+# My Own
+def reverse_list(lst):
+    return lst[::-1]
